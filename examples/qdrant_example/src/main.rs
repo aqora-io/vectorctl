2e1ed5ff43a6271dc05ca39f111c4242aa0dcb68
Angel-Dijoux <angel.dijoux@yahoo.com>
2025-09-30 18:56:09 +0200

    fix: qdrant exemple

--- examples/qdrant_example/src/main.rs
+++ examples/qdrant_example/src/main.rs
@@ -1,4 +1,14 @@
+use vectorctl::{Cli, Parser, VectorTrait};
+
 #[tokio::main]
-async fn main() -> Result<(), vectorctl::CliMigrationError> {
-    vectorctl::run_migrate(qdrant_exemple::Migrator, None).await
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    let dataset_url = cli.database_url;
+    let api_key = cli.api_key;
+
+    let context = vectorctl::Context::new(vectorctl::Backend::new(&dataset_url, api_key)?);
+
+    vectorctl::run_migrate(qdrant_exemple::Migrator, &context)
+        .await
+        .map_err(|err| anyhow::anyhow!(err.to_string()))
 }
